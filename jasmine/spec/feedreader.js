e51b9273512cfefaca94bc1a21ea83518102e22b
Marius De Allie <spectre4300s@gmail.com>
2019-05-27 23:10:01 -0400

    Declare 'url defined' spec in feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,9 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('url defined', function() {
+           
+         })
 
 
         /* TODO: Write a test that loops through each feed
