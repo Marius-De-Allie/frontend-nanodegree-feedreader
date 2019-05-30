ae543c25983b17d8195c7bcb68137d5ae58ee1ab
Marius De Allie <spectre4300s@gmail.com>
2019-05-29 20:23:08 -0400

    Select anchor element with class attrib. of 'menu-icon-link' from DOM and assign to variable 'menuIcon' for use in 'menu-toggle' spec

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,25 +55,24 @@ $(function() {
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
       const body = document.querySelector('body');
-      it('menu hidden', function() {
-        expect(body.className).toContain('menu-hidden');
-      });
-      it('menu toggle', function() {
-
-      });
-    });
-
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+     it('menu hidden', function() {
+       expect(body.className).toContain('menu-hidden');
+     });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+      it('menu toggle', function() {
+        const menuIcon = document.querySelector('.menu-icon-link');
+
+      });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
